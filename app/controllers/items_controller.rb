f5e70e9777fa7e93907bbe57da2a81edd393a975
hizaoni <hizaoni@yahoo.co.jp>
2022-05-05 22:03:41 +0900

    Itemストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,10 +4,19 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    Item.new
   end
 
-  
+  def create
+    Item.cleate(iten_param)
+  end
+
+  private
+
+  def iten_param
+    params.require(:item).permit(:name, :explanation, :price, :category_id, :condition_id, :charge_id, :prefecture_id, :date_id, :explanation, :image).merge(user_id: current_user.id)
+    
+  end
 
 
 
