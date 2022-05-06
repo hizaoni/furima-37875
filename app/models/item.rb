090e5364fa7a269a9a170478dd919dc7bf3827c9
hizaoni <hizaoni@yahoo.co.jp>
2022-05-06 14:03:42 +0900

    validates変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,8 +11,7 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :explanation, presence: true
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                    format: { with: /\A[0-9]+\z/ }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
