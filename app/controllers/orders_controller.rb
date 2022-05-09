2721a5a315b277ca3e76eefbb8b7c55e0bad46d0
hizaoni <hizaoni@yahoo.co.jp>
2022-05-09 20:53:59 +0900

    find_paramsメソッド化

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,13 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: :index
+  before_action :find_item, only: [:index, :create]
   before_action :move_to_idex, only: :index
 
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item
@@ -21,6 +20,10 @@ class OrdersController < ApplicationController
   end
 
   private
+
+  def find_item
+    @item = Item.find(params[:item_id])
+  end
 
   def move_to_idex
     @item = Item.find(params[:item_id])
