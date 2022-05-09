6afdcf8cbb2f33f65596adb4b545ed1919d16ddd
hizaoni <hizaoni@yahoo.co.jp>
2022-05-09 16:41:35 +0900

    validates実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,12 +4,14 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-    @order = Order.new
+    @order_address = OrderAddress.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
-    if @order_address.save
+    if @order_address.valid?
+      @order_address.save
       redirect_to root_path
     else
       render :index
@@ -26,7 +28,7 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.permit(:item_id, :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,7 +3,15 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number
 
-  # バリデーションを書く
+  with_options presence: true do
+    validates :item_id
+    validates :user_id
+    validates :municipality
+    validates :house_number
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :phone_number, numericality: {only_integer: true, message: '"電話番号は半角数字で、ハイフン（-)は除いてください"'},length: {minimum: 10}
+  end
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
